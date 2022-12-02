d4828cac9d7dd257e9f378e6a1298181ced238f3
Ian Breckenridge <62034540+i13e@users.noreply.github.com>
2022-12-02 02:35:01 -0500

    further simplify 2022/02

--- solutions/2022/day_02/solution.py
+++ solutions/2022/day_02/solution.py
@@ -7,25 +7,15 @@ class Solution(StrSplitSolution):
     _day = 2
 
     @answer(12855)
-    def part_1(self) -> int:
-        res, nums = 0, [1, 2, 3]
-        for match in self.input:
-            if ord(match[-1]) - ord(match[0]) in {22, 25}:
-                res += 0 + nums[ord(match[0]) - ord("B")]
-            elif ord(match[-1]) - ord(match[0]) == 23:
-                res += 3 + nums[ord(match[0]) - ord("A")]
-            else:
-                res += 6 + nums[ord(match[0]) - ord("C")]
+    def part_1(self, res=0) -> int:
+        for i in self.input:
+            res += (ord(i[-1]) - ord(i[0]) - 1) % 3 * 3
+            res += ord(i[-1]) - ord("W")
         return res
 
     @answer(13726)
-    def part_2(self) -> int:
-        res, nums = 0, [1, 2, 3]
-        for match in self.input:
-            if match[-1] == "X":
-                res += 0 + nums[ord(match[0]) - ord("B")]
-            elif match[-1] == "Y":
-                res += 3 + nums[ord(match[0]) - ord("A")]
-            else:
-                res += 6 + nums[ord(match[0]) - ord("C")]
+    def part_2(self, res=0) -> int:
+        for i in self.input:
+            res += (ord(i[-1]) + ord(i[0]) - 1) % 3 + 1
+            res += (ord(i[-1]) - ord("X")) * 3
         return res
