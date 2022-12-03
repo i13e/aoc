6a9cdf0dd66eba8fdb1d9095d0877faaf436d301
Ian Breckenridge <62034540+i13e@users.noreply.github.com>
2022-12-03 09:22:56 -0500

    various small improvements update gitignore python template improve date_utils function add result variable to solution template

--- misc/date_utils.py
+++ misc/date_utils.py
@@ -5,13 +5,13 @@ from pathlib import Path
 
 def current_puzzle_year() -> str:
     """
-    if it's on or after nov 30, use this year. Otherwise, use last year.
-
+    if today is on or after nov 30, use this year. Otherwise, use last year.
     Returns a string because math is never done on the result
     """
 
     now = date.today()
-    if now.month == 12 or (now.month == 11 and now.day == 30):
+
+    if now >= date(now.year, 11, 30):
         return str(now.year)
     return str(now.year - 1)
 
@@ -19,7 +19,6 @@ def current_puzzle_year() -> str:
 def next_day(year_dir: Path) -> int:
     """
     Finds the day of the last completed puzzle in a given folder.
-
     Returns 0 by default. Uses int because we add 1 later.
     """
     return max(
