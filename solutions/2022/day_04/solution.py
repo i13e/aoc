1617f1e58cc0085fdb84b5998026c9b683f21e7f
Ian Breckenridge <62034540+i13e@users.noreply.github.com>
2022-12-04 13:21:47 -0500

    update 2022/04

--- solutions/2022/day_04/solution.py
+++ solutions/2022/day_04/solution.py
@@ -11,11 +11,9 @@ class Solution(StrSplitSolution):
     def part_1(self, res=0) -> int:
         for pair in self.input:
             one, two = pair.split(",")
-            s1, e1 = one.split("-")
-            s2, e2 = two.split("-")
-            r1 = set(range(int(s1), int(e1) + 1))
-            r2 = set(range(int(s2), int(e2) + 1))
-            if r1 <= r2 or r1 >= r2:
+            s1, e1 = (int(c) for c in one.split("-"))
+            s2, e2 = (int(c) for c in two.split("-"))
+            if s1 <= s2 <= e2 <= e1 or s2 <= s1 <= e1 <= e2:
                 res += 1
         return res
 
@@ -23,10 +21,8 @@ class Solution(StrSplitSolution):
     def part_2(self, res=0) -> int:
         for pair in self.input:
             one, two = pair.split(",")
-            s1, e1 = one.split("-")
-            s2, e2 = two.split("-")
-            r1 = set(range(int(s1), int(e1) + 1))
-            r2 = set(range(int(s2), int(e2) + 1))
-            if r1 & r2:
+            s1, e1 = (int(c) for c in one.split("-"))
+            s2, e2 = (int(c) for c in two.split("-"))
+            if s2 <= e1 and s1 <= e2:
                 res += 1
         return res
