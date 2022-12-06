330ece25723b2440505d2174f5e61193320e7e8f
Ian Breckenridge <62034540+i13e@users.noreply.github.com>
2022-12-06 14:17:48 -0500

    add day 5 template

---
+++ solutions/2022/day_05/solution.py
@@ -0,0 +1,21 @@
+# prompt: https://adventofcode.com/2022/day/5
+
+# from typing import Tuple
+from ...base import TextSolution, answer
+
+
+class Solution(TextSolution):
+    _year = 2022
+    _day = 5
+
+    # @answer(1234)
+    def part_1(self, res=0) -> int:
+        pass
+
+    # @answer(1234)
+    def part_2(self, res=0) -> int:
+        pass
+
+    # @answer((1234, 4567))
+    # def solve(self) -> Tuple[int, int]:
+    #     pass
