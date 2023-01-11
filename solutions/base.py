3392d50bec2ec981e60ddafc608f91f25e316655
i13e <62034540+i13e@users.noreply.github.com>
2023-01-11 11:26:03 -0500

    Update base.py

--- solutions/base.py
+++ solutions/base.py
@@ -5,10 +5,24 @@ from functools import wraps
 from itertools import product
 from operator import itemgetter
 from pprint import pprint
-from typing import Generic, Iterator, List, Tuple, TypeVar, Union, cast, Any
+
+# pylint: disable=no-name-in-module
+# missing TypeVarTuple and Unpack
+from typing import (
+    Callable,
+    Generic,
+    Iterator,
+    TypeVar,
+    TypeVarTuple,
+    Union,
+    Unpack,
+    cast,
+    final,
+    overload,
+)
 
 
-class InputTypes(Enum):
+class InputTypes(Enum):  # pylint: disable=too-few-public-methods
     # one solid block of text; the default
     TEXT = auto()
     # a single int
@@ -21,27 +35,17 @@ class InputTypes(Enum):
     INTSPLIT = auto()
 
 
-def slow(func):
-    def wrapper(self):
-        if self.slow:
-            return func(self)
-
-        print(
-            f"\nRefusing to run slow function ({func.__name__}). "
-            "Run `./advent` again with the `--slow` flag."
-        )
-        return None
-
-    return wrapper
+# almost always int, but occassionally str; None is fine to disable a part
+ResultType = Union[int, str, None]
 
 
-def print_answer(i: int, res: int):
-    if res is not None:
+def print_answer(i: int, ans: ResultType):
+    if ans is not None:
         print(f"\n== Part {i}")
-        print(f"=== {res}")
+        print(f"=== {ans}")
 
 
-InputType = Union[str, int, List[int], List[str], List[List[int]]]
+InputType = Union[str, int, list[int], list[str], list[list[int]]]
 I = TypeVar("I", bound=InputType)
 
 
@@ -76,25 +80,26 @@ class BaseSolution(Generic[I]):
             raise NotImplementedError("explicitly define number")
         return self._day
 
-    def solve(self) -> Tuple[int, int]:
+    def solve(self) -> tuple[ResultType, ResultType]:
         """
-        Returns the result as a tuple.
+        Returns a 2-tuple with the answers.
             Used instead of `part_1/2` if one set of calculations yields both answers.
         """
         return self.part_1(), self.part_2()  # type: ignore
 
     def part_1(self):
         """
-        Returns the result for part 1 of the puzzle.
+        Returns the answer for part 1 of the puzzle.
             Only needed if there's not a unified solve method.
         """
 
     def part_2(self):
         """
-        Returns the result for part 2 of the puzzle.
+        Returns the answer for part 2 of the puzzle.
             Only needed if there's not a unified solve method.
         """
 
+    @final
     def read_input(self) -> InputType:
         with open(
             os.path.join(
@@ -130,13 +135,22 @@ class BaseSolution(Generic[I]):
 
             raise ValueError(f"Unrecognized input_type: {self.input_type}")
 
+    @final
     def print_solutions(self):
         print(f"\n= Solutions for {self.year} Day {self.day}")
-        p1, p2 = self.solve()
-        print_answer(1, p1)
-        print_answer(2, p2)
-        print()
+        result = self.solve()
+        try:
+            if result:
+                p1, p2 = result
+                print_answer(1, p1)
+                print_answer(2, p2)
+            print()
+        except TypeError as exc:
+            raise ValueError(
+                "unable to unpack tuple from `solve`, got", result
+            ) from exc
 
+    @final
     def pp(self, *obj, newline=False):
         if self.debug:
             for o in obj:
@@ -150,6 +164,7 @@ class BaseSolution(Generic[I]):
             if newline:
                 print()
 
+    @final
     def newline(self):
         if self.debug:
             print()
@@ -171,11 +186,11 @@ class IntSolution(BaseSolution[int]):
     input_type = InputTypes.INTEGER
 
 
-class TSVSolution(BaseSolution[List[List[int]]]):
+class TSVSolution(BaseSolution[list[list[int]]]):
     input_type = InputTypes.TSV
 
 
-class StrSplitSolution(BaseSolution[List[str]]):
+class StrSplitSolution(BaseSolution[list[str]]):
     """
     input is a str[], split by a specified separator (default newline)
         specify self.separator to tweak
@@ -184,7 +199,7 @@ class StrSplitSolution(BaseSolution[List[str]]):
     input_type = InputTypes.STRSPLIT
 
 
-class IntSplitSolution(BaseSolution[List[int]]):
+class IntSplitSolution(BaseSolution[list[int]]):
     """
     input is a int[], split by a specified separator (default newline)
         specify self.separator to tweak
@@ -193,7 +208,59 @@ class IntSplitSolution(BaseSolution[List[int]]):
     input_type = InputTypes.INTSPLIT
 
 
-def answer(res: Union[Any, Tuple[Any, Any]]):
+# https://stackoverflow.com/a/65681955/1825390
+SolutionType = TypeVar("SolutionType", bound=BaseSolution)
+# what the functions that @answer wraps can return
+OutputType = Union[ResultType, tuple[ResultType, ResultType]]
+
+
+def slow(
+    func: Callable[[SolutionType], OutputType]
+) -> Callable[[SolutionType], OutputType]:
+    """
+    A decorator for solution methods that blocks their execution (and returns without error)
+    if the the function is manually marked as "slow". Helpful if running many soultions at once,
+    so one doesn't gum up the whole thing.
+    """
+
+    def wrapper(self: SolutionType):
+        if self.slow or self.use_test_data:
+            return func(self)
+
+        print(
+            f"\nRefusing to run slow function ({func.__name__}). "
+            "Run `./advent` again with the `--slow` flag."
+        )
+        return None
+
+    return wrapper
+
+
+R = TypeVar("R")  # return type generic
+Ts = TypeVarTuple("Ts")  # tuple items generic
+
+
+# see: https://github.com/microsoft/pyright/discussions/4317#discussioncomment-4386187
+@overload
+def answer(
+    ans: tuple[Unpack[Ts]],
+) -> Callable[
+    [Callable[[SolutionType], tuple[Unpack[Ts]]]],
+    Callable[[SolutionType], tuple[Unpack[Ts]]],
+]:
+    ...
+
+
+@overload
+def answer(
+    ans: R,
+) -> Callable[[Callable[[SolutionType], R]], Callable[[SolutionType], R]]:
+    ...
+
+
+def answer(
+    ans: R,
+) -> Callable[[Callable[[SolutionType], R]], Callable[[SolutionType], R]]:
     """
     Decorator to assert the result of the function is a certain thing.
     This is specifically designed to be used on instance methods of BaseSolution.
@@ -208,14 +275,14 @@ def answer(res: Union[Any, Tuple[Any, Any]]):
     ```
     """
 
-    def deco(func):
+    def deco(func: Callable[[SolutionType], R]):
         @wraps(func)
         # uses `self` because that's what's passed to the original solution function
-        def wrapper(self: BaseSolution):
+        def wrapper(self: SolutionType):
             result = func(self)
-            # only assert the result for non-test data
+            # only assert the answer for non-test data
             if not self.use_test_data:
-                assert result == res, f"expected {result=} to equal {res=}"
+                assert result == ans, f"expected {result=} to equal {ans=}"
             return result
 
         return wrapper
@@ -223,7 +290,7 @@ def answer(res: Union[Any, Tuple[Any, Any]]):
     return deco
 
 
-GridPoint = Tuple[int, int]
+GridPoint = tuple[int, int]
 DIRECTIONS = sorted(product((-1, 0, 1), repeat=2), key=itemgetter(1))
 
 
@@ -233,22 +300,30 @@ def neighbors(
     *,
     ignore_negatives: bool = False,
     max_size: int = 0,
-) -> Iterator[Tuple[int, int]]:
+    max_x_size: int = 0,
+    max_y_size: int = 0,
+) -> Iterator[tuple[int, int]]:
     """
     given a point (2-tuple) it yields each neighboring point.
     Iterates from top left to bottom right, skipping any points as described below:
     * `num_directions`: Can get cardinal directions (4), include diagonals (8), or include self (9)
     * `ignore_negatives`: skips points where either value is less than 0
-    * `max_size`: skips points where either value is greater than the max grid size.
+    * `max_DIM_size`: if specified, skips points where the dimension value is greater than the max grid size in that dimension. If doing a 2D-List based (aka `(row,col)` grid) rather than a pure `(x,y)` grid, the max values should be `len(DIM) - 1`. Is mutually exclusive with `max_size`.
+    For a 2D list-based grid, neighbors will come out in (row, col) format.
     """
-    assert num_directions in [4, 8, 9]
+    assert num_directions in {4, 8, 9}
+    # one or the other
+    if max_size:
+        assert not (max_x_size or max_y_size)
+    if max_x_size or max_y_size:
+        assert not max_size
 
     for dx, dy in DIRECTIONS:
         if num_directions == 4 and dx and dy:
             # diagonal; skip
             continue
 
-        if num_directions == 8 and not (dx or dy):
+        if num_directions != 9 and not (dx or dy):
             # skip self
             continue
 
@@ -259,6 +334,12 @@ def neighbors(
             continue
 
         if max_size and (rx > max_size or ry > max_size):
+            continue
+
+        if max_x_size and (rx > max_x_size):
+            continue
+
+        if max_y_size and (ry > max_y_size):
             continue
 
         yield (rx, ry)
