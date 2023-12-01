32701c500ccf107ad8a51ae7afa4d476e1b7b555
Ian Breckenridge <62034540+i13e@users.noreply.github.com>
2023-12-01 15:01:02 -0500

    refactor: 2023 day_01

--- solutions/2023/day_01/solution.py
+++ solutions/2023/day_01/solution.py
@@ -1,7 +1,5 @@
 # puzzle prompt: https://adventofcode.com/2023/day/1
 
-from itertools import combinations
-
 from ...base import StrSplitSolution, answer
 
 
@@ -9,29 +7,23 @@ class Solution(StrSplitSolution):
     _year = 2023
     _day = 1
 
+    def parse(self, s: str, part_2: bool = False) -> int:
+        words = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+        for i, word in enumerate(words * part_2, 1):
+            s = s.replace(word, f"{word[0]}{i}{word[-1]}")
+
+        a = b = ""
+        for c in filter(str.isdigit, s):
+            a, b = a or c, c
+        return int(a + b)
+
     @answer(54304)
-    def part_1(self) -> int | str:
-        res = 0
-        for line in self.input:
-            nums = [int(n) for n in line if n.isdigit()]
-            res += nums[0] * 10 + nums[-1]
-        return res
+    def part_1(self) -> int:
+        return sum(self.parse(line) for line in self.input)
 
     @answer(54418)
-    def part_2(self) -> int | str:
-        words = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-        rmap = {w: i for i, w in enumerate(words, 1)}
-        res = 0
-        for line in self.input:
-            nums = []
-            for i, j in combinations(range(len(line) + 1), 2):
-                cur = line[i:j]
-                if cur.isdigit():
-                    nums.append(int(cur))
-                elif cur in rmap:
-                    nums.append(rmap[cur])
-            res += nums[0] * 10 + nums[-1]
-        return res
+    def part_2(self) -> int:
+        return sum(self.parse(line, True) for line in self.input)
 
     # @answer((1234, 5678))
     # def solve(self) -> tuple[int | str, int | str]:
